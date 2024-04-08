7ff6994ad641d22c0d2d02fb398e93df1759549f
chanb <chanbi741@gmail.com>
2024-04-08 17:06:05 +0900

    init project

---
+++ src/main/java/dockerproject/dockerproject/controller/MemberOpenApiController.java
@@ -0,0 +1,17 @@
+package dockerproject.dockerproject.controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/open-api/members")
+@RestController
+public class MemberOpenApiController {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome~";
+    }
+}
